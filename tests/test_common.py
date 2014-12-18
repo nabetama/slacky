b5c7f71a173acf2d9c85c9075af1f5a833d5358c
nabetama <mao.nabeta@gmail.com>
2014-12-18 21:59:29 +0900

    Add test channel.

--- tests/test_common.py
+++ tests/test_common.py
@@ -23,6 +23,7 @@ class TestSlack(object):
         else:
             print('Authorization token not detected! The token is pulled from '\
                 '~/.slack, /etc/slack, or the environment variable SLACK_TOKEN.')
+        self.test_channel = self.config.get('Slack', 'test-channel')
 
     def set_up_slack(self):
         self.slack = slack.Slack(self.access_token)
