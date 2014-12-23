5d5ee4d70662445ea99c3ca71443e51bbcc239e1
nabetama <mao.nabeta@gmail.com>
2014-12-24 02:08:03 +0900

    Update before test process.

--- tests/test_common.py
+++ tests/test_common.py
@@ -17,15 +17,15 @@ class TestSlack(object):
         self.config.read(search_paths)
 
         if self.config.has_section('Slack'):
-            self.access_token = self.config.get('Slack', 'token')
+            self.access_token      = self.config.get('Slack', 'token')
+            self.test_channel      = self.config.get('Slack', 'test-channel')
+            self.test_user         = self.config.get('Slack', 'test-user')
+            self.test_channel_name = self.config.get('Slack', 'test-channel-name')
         elif 'SLACK_TOKEN' in os.environ:
             self.access_token = os.environ['SLACK_TOKEN']
         else:
             print('Authorization token not detected! The token is pulled from '\
                 '~/.slack, /etc/slack, or the environment variable SLACK_TOKEN.')
-        self.test_channel      = self.config.get('Slack', 'test-channel')
-        self.test_user         = self.config.get('Slack', 'test-user')
-        self.test_channel_name = self.config.get('Slack', 'test-channel-name')
 
     def set_up_slack(self):
         self.slack = Slacky(self.access_token)
