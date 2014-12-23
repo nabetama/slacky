edcfcee1bb601945b654b2d8c3b6695edf255fc2
nabetama <mao.nabeta@gmail.com>
2014-12-24 00:50:40 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='pyslack',
+    version="0.0.1",
+    description="Package for Slack's API",
+    author='Mao Nabeta',
+    author_email='mao.nabeta@gmail.com',
+    url='https://github.com/nabetama/pyslack',
+    packages=['pyslack'],
+    install_requires=['requests', 'six'],
+    provides=['slack'],
+    keywords='slack',
+    classifiers=[ # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications :: Chat',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+    )
