0ce70d92c2ccc98d4e3d1275f57b16b603cf0480
nabetama <mao.nabeta@gmail.com>
2015-01-01 23:41:28 +0900

    Timeline.

--- slacky/__init__.py
+++ slacky/__init__.py
@@ -141,3 +141,12 @@ class Slacky(object):
     @property
     def users(self):
         return self.fromurl('https://slack.com/api/users', token=self.token)
+
+    def timeline(self, channel_name='general', count=100):
+        messages = self.channels.timeline(channel_name, count=count)
+        for message in messages:
+            print("@%s: {%s}"%(
+                self.users.get_name_by_id(message.user),
+                message.text,
+                )
+            )
